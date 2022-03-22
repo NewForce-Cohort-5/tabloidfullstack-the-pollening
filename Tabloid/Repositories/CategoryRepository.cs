75b644a17f96b62c4db96dc2bdeffabfaf0b9e80
john gallagher <127highstreet@gmail.com>
2022-03-22 15:37:01 -0400

    saving

---
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Tabloid.Models;
+using Tabloid.Utils;
+
+namespace Tabloid.Repositories
+{
+    public class CategoryRepository : BaseRepository
+    {
+        public CategoryRepository(IConfiguration configuration) : base(configuration) { }
+    }
+
+
+
+}
