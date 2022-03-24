5ebee843041c7798f67061b33ff6cf38ebe2e564
cresuta <60229891+cresuta@users.noreply.github.com>
2022-03-24 10:35:09 -0400

    fixed navbar layout where Posts was out of place

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -27,13 +27,15 @@ export default function Header() {
             {isLoggedIn &&
               <NavItem>
                 <NavLink tag={RRNavLink} to="/">Home</NavLink>
-                <NavLink tag={RRNavLink} to="/posts">Posts</NavLink>
               </NavItem>
             }
           </Nav>
           <Nav navbar>
             {isLoggedIn &&
               <>
+                <NavItem>
+                  <NavLink tag={RRNavLink} to="/posts">Posts</NavLink>
+                </NavItem>
                 <NavItem>
                   <a aria-current="page" className="nav-link"
                     style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
