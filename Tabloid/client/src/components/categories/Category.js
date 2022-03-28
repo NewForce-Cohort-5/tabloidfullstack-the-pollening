345c13169c81a64f199b087370a686c7ac3c7b3a
john gallagher <127highstreet@gmail.com>
2022-03-28 15:28:48 -0400

    saving

--- Tabloid/client/src/components/categories/Category.js
+++ Tabloid/client/src/components/categories/Category.js
@@ -1,11 +1,24 @@
 import React from "react";
 import {ListGroup} from "react-bootstrap";
-
+import Button from 'react-bootstrap/Button';
 import {Link } from "react-router-dom";
+import "./Category.css";
+import { CategoryContext } from "../../providers/CategoryProvider";
+
 //we're just using the Card component that comes with reactstrap to organize some of the category details.
 export const Category = ({CategoryProp}) => {
+    const onClickHandler = () => {
+        alert("Are you sure you want to delete this?")
+
+    }
+
     return (
-        <ListGroup.Item id={CategoryProp.id}> {CategoryProp.name}</ListGroup.Item>
+        
+        <ListGroup.Item id={CategoryProp.id} className="category__flex-container" ><div> {CategoryProp.name}</div>
+        <div> <Button variant="secondary" size="md" onClick={onChange={onClickHandler}}> 
+                Delete
+             </Button></div>
+        </ListGroup.Item>
     )
 }
 
