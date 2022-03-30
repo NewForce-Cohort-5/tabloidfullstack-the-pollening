9fc99c4f0067821481c53dcb1ea140ecac1aa7e7
cresuta <60229891+cresuta@users.noreply.github.com>
2022-03-30 12:01:50 -0400

    quick edit to update pr

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -92,6 +92,48 @@ namespace Tabloid.Repositories
             }
         }
 
+        public List<Post> GetAllPostsByUserId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"      
+                       SELECT p.Id AS PostId, p.Title, p.Content, 
+                              p.ImageLocation AS PostImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              c.[Name] AS CategoryName,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS UserProfileImage,
+                              u.UserTypeId, 
+                              ut.[Name] AS UserTypeName
+                         FROM Post p
+                              LEFT JOIN Category c ON p.CategoryId = c.id
+                              LEFT JOIN UserProfile u ON p.UserProfileId = u.id
+                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                        WHERE p.UserProfileId = @id
+                        ORDER BY p.PublishDateTime DESC";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    var posts = new List<Post>();
+
+                    while (reader.Read())
+                    {
+                        posts.Add(NewPostFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return posts;
+                }
+            }
+        }
+
         public void Add(Post post)
         {
             using (var conn = Connection)
